ff437e7ec48f7d8d70f77c4b5d17531f6eb84450
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2024-05-05 10:36:25 -0300

    first commit

---
+++ src/main/java/br/com/postechfiap/jlapppagamento/JlappPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.jlapppagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlappPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlappPagamentoApplication.class, args);
+	}
+
+}
